a1dc2ac0c0c11a4056b25256ebb2982c829a67bc
woong <inwoong100@gmail.com>
2022-11-02 00:18:47 +0900

    feat: set emotion, twin.macro, tailwindcss

--- SeniorBank/src/App.jsx
+++ SeniorBank/src/App.jsx
@@ -1,7 +1,30 @@
-import './App.css';
+import { css } from '@emotion/react';
+import tw, { styled } from 'twin.macro';
+
+const Caption = tw.p`
+font-light w-full
+md:font-thin md:whitespace-normal md:text-left 
+`;
+const Icon = styled.div(({ index }) => [
+  tw`
+   md:flex-shrink-0 md:mt-20 md:leading-3
+`,
+  css`
+    position: relative;
+    &::before {
+      content: '0${index}';
+      padding: ${index};
+    }
+  `,
+]);
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Caption>1234</Caption>
+      <Icon index={2} />
+    </div>
+  );
 }
 
 export default App;
